7c4bd8469bfe0e9fcebcb71ac9d150461e7ae532
Shrusti <dvsaraswathi19@gmail.com>
2024-08-07 22:48:55 +0530

    new

---
+++ data/profile.js
@@ -0,0 +1,119 @@
+const profileData = {
+  title: "Resume",
+  name: "Shrusti D.V",
+  sub_title: "Software Engineer",
+  logoURL: "assets/images/dp.jpg",
+  about: {
+    intro: ` Software Engineer with 6 years of frontend development experience. Expert in building high-performance UI and seamless UX. Experienced in leading projects, migrating legacy systems, and creating reusable components. Skilled in A/B testing, cross-platform compatibility, and responsive design. Effective mentor and collaborator dedicated to continuous learning and innovation.`,
+    contact: {
+      email: "dvshrusti@gmail.com",
+      phone: "+91-6361762490",
+      address: "Bangalore, INDIA",
+    },
+  },
+  links: [
+    {
+      title: "LinkedIn",
+      src: "https://www.linkedin.com/in/imvpn22",
+      iconClass: "fa-brands fa-linkedin-in",
+    },
+    {
+      title: "Github",
+      src: "https://github.com/imvpn22",
+      iconClass: "fa-brands fa-github",
+    },
+    {
+      title: "Codepen",
+      src: "https://codepen.io/imvpn22",
+      iconClass: "fa-brands fa-codepen",
+    },
+  ],
+  languages: [
+    {
+      title: "Languages",
+      value: "English, Kannada, Hindi,Malayalam,Tamil, Tulu,Telugu ,Beary , Konkani",
+    },],
+  skills: [
+    {
+      title: "Languages",
+      value: "HTML, CSS, JavaScript , C , C++ , Python , Java ,",
+    },
+    {
+      title: "Frameworks/Libraries",
+      value: "ReactJS, React-Native, Redux, NodeJS-Express, Angular",
+    },
+    {
+      title: "Others",
+      value: "Git, AWS",
+    },
+  ],
+  experiences: [
+    {
+      organization: "Gameskraft",
+      title: "Senior Software Engineer",
+      date: "Dec 2022 - Present",
+      details: [
+        `As part of the <strong>RummyCulture App</strong> team, worked on the development of core features like <strong>TDS summary & certificate</strong>, <strong>game join & re-join flow</strong> improvements,
+        <strong>scorecard</strong>, <strong>withdrawal flow</strong>, <strong>user referral flow</strong>, and <strong>game bubble state</strong> optimizations.
+        I ensured end-to-end responsibility, focusing on <strong>pixel-perfect UI</strong>, smooth UX, user testing, <strong>A/B experiments</strong>, cross-platform compatibility,
+        and timely delivery.`,
+        `Implemented the <strong>FT/UX</strong> (First Time User Experience) flow, boosting day1 metrics of Registration to Add Cash and Registration to Cash Game by around <strong>5%</strong>.`,
+        `Integrated in-house <strong>help center SDK</strong>, offering self-help features like guided app core flows and callback scheduling for users, leading to a <strong>reduction in customer call volume</strong>.`,
+        `Initiated the standardization of a React-Native <strong>custom UI library</strong>, encompassing reusable components, utilities, higher-order components, hooks, and animations for versatile project needs.`,
+        `Addressed live issues such as App crashes, JS crashes, and ANRs using <strong>Sentry</strong> and <strong>Firebase Crashlytics</strong>. Mentored juniors through knowledge-sharing and code-review sessions to ensure bug-free code and optimal performance.`,
+      ],
+    },
+  ],
+  projects: [
+    {
+      title: "Restaurant Review",
+      duration: "Jun - Nov 2018",
+      link: "https://github.com/imvpn22/restaurant-review-pwa",
+      desc: `A web app to list, view and add reviews of restaurants.
+      The app includes filtering based on location, cuisine, and rating.
+      This project was a part of Udacity's <strong>Mobile Web Specialist</strong> nano degree program.
+      Developed using <strong>HTML5</strong>, <strong>CSS3</strong> and <strong>JavaScript</strong>
+      with Progressive Web-Application standards including full-responsiveness,
+      offline support, and improved accessibility.`,
+    },
+    {
+      title: "WhiteBoard",
+      duration: "Jun - Aug 2017",
+      link: "https://github.com/imvpn22/whiteboard",
+      desc: `A simple <strong>web-app for collaborative brainstorming sessions</strong>.
+      The app includes a real-time messaging and sketching platform for collaboration with Peers.
+      It also has features to create groups(teams) and add/remove members.
+      Developed using <strong>Node.js-Express</strong> with server-side-templating (EJS),
+      <strong>CSS</strong>, <strong>JavaScript</strong> and <strong>Socket.io</strong>.`,
+    },
+  ],
+  education: [
+    {
+      alma: "Sheshadripuram First Grade College , Bangalore ",
+      duration: "2021 - 2023",
+      std: "B.C.A",
+      score: "82.00%",
+    },
+    {
+      alma: "St. Agnes , Mangalore",
+      duration: "2018 - 2020",
+      std: "Class XI-XII ()",
+      score: "89.80%",
+    },
+  ],
+  certifications: [
+    {
+      desc: `<strong>Mobile Web Specialist Nanodegree</strong> by <strong>Udacity</strong>.
+      (<a target='_blank' rel='noreferrer' href='https://confirm.udacity.com/RLMHXAWZ'>https://goo.gl/RGRzVn</a>)`,
+      date: "May - Nov 2018",
+    },
+    {
+      desc: `<strong>Introduction to Modern Application Development</strong>, a MOOC by <em> NPTEL (IIT Madras)
+      and Hasura</em>. Ranked at <strong> top 10 percentile </strong> among more than 2500 candidates.
+       (<a target='_blank' rel='noreferrer' href='https://nptel.ac.in/noc/E_Certificate/linkedin/noc17-cs06/NPTEL17CS0626270067AN.jpg'>
+       https://goo.gl/X3HEdR</a>)`,
+      date: "Jan - Mar 2017",
+    },
+  ],
+  events: [],
+};
